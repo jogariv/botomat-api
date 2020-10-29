939456ba84f2661c8ae2c0170ac5aedeef43fb4c
Jose G. Rivera <jogariv@Joses-Mac-mini.local>
2020-10-29 13:12:53 -0400

    change files to adapt to heroku

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/robots', (req, res) => {
     });
 });
  
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
