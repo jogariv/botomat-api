64eff386272ad755be7fa4ad6a82eca5849bfa8b
Jose G. Rivera <jogariv@Joses-Mac-mini.local>
2020-10-29 12:54:58 -0400

    checkpoint

---
+++ data.js
@@ -0,0 +1,204 @@
+
+const robos = [
+    {
+        id: 'Uni',
+        avatarPic: '/avatars/robo_01.svg',
+        type: 'UNIPEDAL',
+        tasks: [
+            {
+                id: 'Dishes',
+                description: 'do the dishes',
+                eta: 1000,
+            },
+            {
+                id: 'Sweep',
+                description: 'sweep the house',
+                eta: 3000,
+            },
+            {
+                id: 'Laundry',
+                description: 'do the laundry',
+                eta: 10000,
+            },
+            {
+                id: 'Recycling',
+                description: 'take out the recycling',
+                eta: 4000,
+            },
+            {
+                id: 'Sammich',
+                description: 'make a sammich',
+                eta: 7000,
+            },
+            {
+                id: 'CarWash',
+                description: 'wash my Camaro!',
+                eta: 7000,
+            },
+        ],
+    },
+    {
+        id: 'Bibi',
+        avatarPic: '/avatars/robo_02.svg',
+        type: 'BIPEDAL',
+        tasks: [
+            {
+                id: 'Dishes',
+                description: 'do the dishes',
+                eta: 1000,
+            },
+            {
+                id: 'Sweep',
+                description: 'sweep the house',
+                eta: 3000,
+            },
+            {
+                id: 'Laundry',
+                description: 'do the laundry',
+                eta: 10000,
+            },
+            {
+                id: 'Recycling',
+                description: 'take out the recycling',
+                eta: 4000,
+            },
+            {
+                id: 'Sammich',
+                description: 'make a sammich',
+                eta: 7000,
+            },
+        ],
+    },
+    {
+        id: 'Quadri',
+        avatarPic: '/avatars/robo_03.svg',
+        type: 'QUADRUPEDAL',
+        tasks: [
+            {
+                id: 'Dishes',
+                description: 'do the dishes',
+                eta: 1000,
+            },
+            {
+                id: 'Sweep',
+                description: 'sweep the house',
+                eta: 3000,
+            },
+            {
+                id: 'DogBath',
+                description: 'give the dog a bath',
+                eta: 14500,
+            },
+            {
+                id: 'Bake',
+                description: 'bake some cookies',
+                eta: 8000,
+            },
+            {
+                id: 'CarWash',
+                description: 'wash the car',
+                eta: 20000,
+            },
+        ],
+    },
+    {
+        id: 'Arry',
+        avatarPic: '/avatars/robo_04.svg',
+        type: 'ARACHNID',
+        tasks: [
+            {
+                id: 'Dishes',
+                description: 'do the dishes',
+                eta: 1000,
+            },
+            {
+                id: 'Sweep',
+                description: 'sweep the house',
+                eta: 3000,
+            },
+            {
+                id: 'Laundry',
+                description: 'do the laundry',
+                eta: 10000,
+            },
+            {
+                id: 'Recycling',
+                description: 'take out the recycling',
+                eta: 4000,
+            },
+            {
+                id: 'Sammich',
+                description: 'make a sammich',
+                eta: 7000,
+            },
+        ],
+    },
+    {
+        id: 'Radi',
+        avatarPic: '/avatars/robo_05.svg',
+        type: 'RADIAL',
+        tasks: [
+            {
+                id: 'DogBath',
+                description: 'give the dog a bath',
+                eta: 14500,
+            },
+            {
+                id: 'Bake',
+                description: 'bake some cookies',
+                eta: 8000,
+            },
+            {
+                id: 'CarWash',
+                description: 'wash the car',
+                eta: 20000,
+            },
+            {
+                id: 'Recycling',
+                description: 'take out the recycling',
+                eta: 4000,
+            },
+            {
+                id: 'Sammich',
+                description: 'make a sammich',
+                eta: 7000,
+            },
+        ],
+    },
+    {
+        id: 'Nauti',
+        avatarPic: '/avatars/robo_06.svg',
+        type: 'AERONAUTICAL',
+        tasks: [
+            {
+                id: 'Dishes',
+                description: 'do the dishes',
+                eta: 1000,
+            },
+            {
+                id: 'Sweep',
+                description: 'sweep the house',
+                eta: 3000,
+            },
+            {
+                id: 'DogBath',
+                description: 'give the dog a bath',
+                eta: 14500,
+            },
+            {
+                id: 'Bake',
+                description: 'bake some cookies',
+                eta: 8000,
+            },
+            {
+                id: 'CarWash',
+                description: 'wash the car',
+                eta: 20000,
+            },
+        ],
+    }
+];
+
+module.exports = {
+    robos
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const response = require('./data');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+ 
+const app = express();
+ 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Bot-O-Mat Api is up and running!'
+    });
+});
+
+app.get('/robots', (req, res) => {
+    res.json({
+        response
+    });
+});
+ 
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
